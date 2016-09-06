f2075ebd35bab0313aa8a3b2eae1883a251ab0ad
CoderB <noodoggy@gmail.com>
2016-09-06 12:37:11 -0400

    Add project files.

---
+++ CBLSummer09052016Budgeter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CBLSummer09052016Budgeter.MvcApplication" Language="C#" %>
---
+++ CBLSummer09052016Budgeter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CBLSummer09052016Budgeter.Startup))]
+namespace CBLSummer09052016Budgeter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
